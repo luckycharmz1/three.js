b1e3dffb206b4c9cd794050b3d90e8ceaf874766
Charmaine Washington <98441068+luckycharmz1@users.noreply.github.com>
2022-12-22 11:31:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -14,5 +14,12 @@ const sizes = {
 }
 
 //Camera 
-const camera = new THREE.PerspectiveCamera(75)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+console.log(canvas)
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
