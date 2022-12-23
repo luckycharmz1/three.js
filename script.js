3abc599a5aa0e2f55b2653fbbd5ecef0d0437327
Charmaine Washington <98441068+luckycharmz1@users.noreply.github.com>
2022-12-23 15:40:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ const sizes = {
 //Camera 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.x = 2
 scene.add(camera)
 
 // Renderer
