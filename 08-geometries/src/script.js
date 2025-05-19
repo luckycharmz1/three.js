5473d21ff798ebfd1618d8791b605e5ed79af65c
Charmaine Washington <cwashington2011@outlook.com>
2025-05-18 19:49:05 -0700

    Update script.js

--- 08-geometries/src/script.js
+++ 08-geometries/src/script.js
@@ -17,8 +17,16 @@ const geometry = new THREE.BufferGeometry()
 
 //const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
 
-const count = 50
+const count = 5000
 const positionsArray = new Float32Array(count * 3 * 3)
+
+for (let i = 0; i < count * 3 * 3; i++)
+{
+    positionsArray[i] = (Math.random() - 0.5) * 4
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
 
 //geometry.setAttribute('position', positionsAttribute)
 
