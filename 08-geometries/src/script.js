55534cf21d90e8a1a5b40132e3f7d264197e9429
Charmaine Washington <cwashington2011@outlook.com>
2025-05-18 19:32:33 -0700

    three.js lesson

--- 08-geometries/src/script.js
+++ 08-geometries/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { WireframeGeometry } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
@@ -11,8 +12,22 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+//const geometry = new THREE.BoxGeometry(1, 1, 1, 2 ,2, 2)
+const geometry = new THREE.BufferGeometry()
+
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+
+const count = 50
+const positionsArray = new Float32Array(count * 3 * 3)
+
+geometry.setAttribute('position', positionsAttribute)
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0xff0000,
+    wireframe: true
+ })
+
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
